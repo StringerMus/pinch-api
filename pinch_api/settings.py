70f1234d8f276c94624c50419edf787bebdccdd8
Musaddek Ali <mussali.one@gmail.com>
2024-09-10 17:39:36 +0000

    Cloudinary bug fixed in settings, Cloud name can be located.

--- pinch_api/settings.py
+++ pinch_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.path.exists('env.py'):
+if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
     import env
 
 CLOUDINARY_STORAGE = {
