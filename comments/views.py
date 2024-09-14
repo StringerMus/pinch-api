84212b9131ee21d8f2afbedbe43b9f67c7ad2be4
Musaddek Ali <mussali.one@gmail.com>
2024-09-14 21:06:26 +0000

    Create comment views to create, list, update and delete comments.

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,22 @@
 from django.http import Http404
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 from pinch_api.permissions import IsOwnerOrReadOnly
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
