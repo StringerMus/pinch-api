83b8782405b5885294d28929aa66e990436610f2
Musaddek Ali <mussali.one@gmail.com>
2024-09-21 12:24:22 +0000

    Amend views import of settings JWT.

--- pinch_api/views.py
+++ pinch_api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+#from .settings import (
+#    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+#    JWT_AUTH_SECURE,
+#)
+
+from .settings import REST_AUTH
+JWT_AUTH_COOKIE = REST_AUTH['JWT_AUTH_COOKIE']
+JWT_AUTH_REFRESH_COOKIE = REST_AUTH['JWT_AUTH_REFRESH_COOKIE']
+JWT_AUTH_SAMESITE = REST_AUTH['JWT_AUTH_SAMESITE']
+JWT_AUTH_SECURE = REST_AUTH['JWT_AUTH_SECURE']
 
 
 @api_view()
