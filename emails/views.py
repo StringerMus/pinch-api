33213dd26ba0e673cc7c55dc74942ccbd46074a7
Musaddek Ali <mussali.one@gmail.com>
2025-01-11 14:26:42 +0000

    Add import of api_view and Response in email views.py.

--- emails/views.py
+++ emails/views.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from django.core.mail import send_mail
 from .serializers import EmailSerializer
 from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 @api_view(['POST'])
 def send_email_view(request):
