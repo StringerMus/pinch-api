237e5c4c36d9a0d640a89f8c02e33b54688c316f
Musaddek Ali <mussali.one@gmail.com>
2025-01-11 15:44:36 +0000

    Amend API path in CORS.

--- pinch_api/settings.py
+++ pinch_api/settings.py
@@ -71,7 +71,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://pinch-5e6e24dd12fc.herokuapp.com",
-    "https://pinch-api-f947cf5f7bdc.herokuapp.com/send-email",
+    "https://pinch-api-f947cf5f7bdc.herokuapp.com",
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
