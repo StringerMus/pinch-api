af7babec05c785f0e9f58a73ccc2c7cf0303b855
Musaddek Ali <mussali.one@gmail.com>
2025-01-11 15:43:08 +0000

    Add send-email path to CORS origin.

--- pinch_api/settings.py
+++ pinch_api/settings.py
@@ -71,6 +71,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://pinch-5e6e24dd12fc.herokuapp.com",
+    "https://pinch-api-f947cf5f7bdc.herokuapp.com/send-email",
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
